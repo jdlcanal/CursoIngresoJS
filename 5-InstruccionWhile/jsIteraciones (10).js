96fe6fdc3cfebad9b3ef67b1053ff4f786cae88f
jdlcanal <jdlcanal@hotmail.com>
2020-02-25 22:29:30 -0300

    25/2

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,44 +1,128 @@
 function mostrar()
 {
 
-	var contador=0;
-	var sumaPositivos=0;
-	var sumaNegativos=0;
-	var contadorPositivos=0;
-	var contadorNegativos=0;
-	var contadorCeros=0;
-	var contadorPares=0;
+	var contador;
+	var respuesta;
+	var notaAlumno;
+	var sexoAlumno;
+	var edadAlumno;
+	var nombreAlumno;
+	var acumulador;
+	var promedioNotas;
+	var mejorNota;
+	var peorNota;
+	var bandera;
+	var contadorDeMujeres;
+
+	mejorNotaHombres=-1;
+
+	contador=0;//#1 inicializar para ingresar
+	acumulador=0;
+	contadorDeMujeres=0;
+	respuesta="si";
 	//declarar contadores y variables 
 	
-while(contador > -1) {
-		contador ++;
-		var numero;
-		numero = prompt ("Ingrasar Nùmeros");
-		numero = parseInt (numero);
-		if (isNaN(numero)) {
-			alert ("No Ingresar Más Números");
-			break;
-		}
-
-
-		if (numero > 0) {
-			
-			sumaPositivos += numero;
+	//while(contador<5)//#2 defino la logica
+	while(respuesta=="si")
+	{
+		contador=contador+1;//#3 cambio la variable de la logica
 		
-			contadorPositivos ++;
-		} else if (numero < 0) {
 
-			sumaNegativos += numero;
-			contadorNegativos ++;
-		} else {
-			contadorCeros ++;
+		nombreAlumno=prompt("ingrese su nombre");
+
+
+		notaAlumno=prompt("ingrese el "+contador+"º numero :");
+		notaAlumno=parseInt(notaAlumno);
+		while(notaAlumno<0 || notaAlumno>10)//bien
+		{
+			notaAlumno=prompt("error,reingrese el "+contador+"º numero :");
+			notaAlumno=parseInt(notaAlumno);
 		}
-		if (numero % 2 == 0) {
-			contadorPares ++;
+
+		sexoAlumno= prompt("ingrese f o m");
+		while(sexoAlumno!="f" && sexoAlumno!="m")//casi bien
+		{
+			sexoAlumno= prompt("ingrese f o m");
 		}
+
+		edadAlumno=prompt("ingrese el "+contador+"º edad :");
+		edadAlumno=parseInt(edadAlumno);
+		while(edadAlumno<0 || edadAlumno>100)//bien
+		{
+			edadAlumno=prompt("error,reingrese el "+contador+"º edad :");
+			edadAlumno=parseInt(edadAlumno);
+		}
+		edadAlumno=prompt("ingrese el "+contador+"º edad :");
+		edadAlumno=parseInt(edadAlumno);
+		while(edadAlumno<0 || edadAlumno>100)//bien
+		{
+			edadAlumno=prompt("error,reingrese el "+contador+"º edad :");
+			edadAlumno=parseInt(edadAlumno);
+		}
+
+
+		//termino el ingreso
+
 		
-	}	
+		if(sexoAlumno=="f")
+		{
+			contadorDeMujeres= contadorDeMujeres+1;
 
-	document.write("Suma de Positivos "+ sumaPositivos);
+		}else
+		{
+			if(mejorNotaHombres==-1)
+			{
+				mejorNotaHombres=notaAlumno;
+				
+				
+			}
+			else
+			{
+				if(notaAlumno>mejorNotaHombres)
+				{
+					mejorNotaHombres=notaAlumno;
 
+				}
+
+			}
+
+		}
+
+
+
+
+		if(contador==1)
+		{
+			mejorNota=notaAlumno;
+			peorNota=notaAlumno;
+			edadPrimerAlumno=edad;
+		}
+		else
+		{
+
+			if(notaAlumno>mejorNota)
+			{
+				mejorNota=notaAlumno;
+				mejorNotaNombre=nombre;
+
+			}
+
+			if(notaAlumno<peorNota)
+			{
+				peorNota=notaAlumno;
+			}
+		}
+
+
+		acumulador=acumulador+notaAlumno;
+		respuesta=prompt("si para seguir");
+	}//fin while 
+
+	promedioNotas=acumulador/contador;
+
+	console.log("contador:"+contador);
+	console.log("acumulador:"+acumulador);
+	console.log("mejorNota:"+mejorNota);
+	console.log("peorNota:"+peorNota);
+	
 }//FIN DE LA FUNCIÓN
